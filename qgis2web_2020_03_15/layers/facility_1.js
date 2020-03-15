3a12d80ced7aa2fc9fa87d2d4e1bc4a400909ccd
UBUKAWA Taro <59204379+ubukawa@users.noreply.github.com>
2020-03-15 00:41:43 -0400

    added map

---
+++ qgis2web_2020_03_15/layers/facility_1.js
@@ -0,0 +1,27 @@
+var json_facility_1 = {
+"type": "FeatureCollection",
+"name": "facility_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null, "name": "William E Cottle School", "type": "school" }, "geometry": { "type": "Point", "coordinates": [ -73.814880558162073, 40.941041413654794 ] } },
+{ "type": "Feature", "properties": { "id": null, "name": "Tuckahoe Station", "type": "station" }, "geometry": { "type": "Point", "coordinates": [ -73.8285276286082, 40.950585181318644 ] } },
+{ "type": "Feature", "properties": { "id": null, "name": "Tuckahoe library", "type": "public facility" }, "geometry": { "type": "Point", "coordinates": [ -73.825740839448031, 40.952632305789081 ] } },
+{ "type": "Feature", "properties": { "id": null, "name": "H Mart", "type": "shop" }, "geometry": { "type": "Point", "coordinates": [ -73.842758342017277, 40.95971670227555 ] } },
+{ "type": "Feature", "properties": { "id": null, "name": "shoprite", "type": "shop" }, "geometry": { "type": "Point", "coordinates": [ -73.858714547091964, 40.953532038539755 ] } },
+{ "type": "Feature", "properties": { "id": null, "name": "Costco", "type": "shop" }, "geometry": { "type": "Point", "coordinates": [ -73.861806582934975, 40.974737845593978 ] } },
+{ "type": "Feature", "properties": { "id": null, "name": "Stew Leonard`s", "type": "shop" }, "geometry": { "type": "Point", "coordinates": [ -73.864467599156342, 40.974167025692765 ] } },
+{ "type": "Feature", "properties": { "id": null, "name": "Constitution park", "type": "park" }, "geometry": { "type": "Point", "coordinates": [ -73.824033012521085, 40.952823353270418 ] } },
+{ "type": "Feature", "properties": { "id": null, "name": "Park", "type": "park" }, "geometry": { "type": "Point", "coordinates": [ -73.826708160533343, 40.953978164822544 ] } },
+{ "type": "Feature", "properties": { "id": null, "name": "Bronxville station", "type": "station" }, "geometry": { "type": "Point", "coordinates": [ -73.834920144209605, 40.941327501230653 ] } },
+{ "type": "Feature", "properties": { "id": null, "name": "Trader Joe's", "type": "shop" }, "geometry": { "type": "Point", "coordinates": [ -73.804868890918243, 40.972385182267296 ] } },
+{ "type": "Feature", "properties": { "id": null, "name": "Eastchester Public Library", "type": "public facility" }, "geometry": { "type": "Point", "coordinates": [ -73.811801947520607, 40.957279192170745 ] } },
+{ "type": "Feature", "properties": { "id": null, "name": "CVS", "type": "shop" }, "geometry": { "type": "Point", "coordinates": [ -73.814320821478802, 40.954295700858552 ] } },
+{ "type": "Feature", "properties": { "id": null, "name": "Stop & Shop", "type": "shop" }, "geometry": { "type": "Point", "coordinates": [ -73.813536376235447, 40.955537636311917 ] } },
+{ "type": "Feature", "properties": { "id": null, "name": "Tuckahoe High School", "type": "school" }, "geometry": { "type": "Point", "coordinates": [ -73.812500044904738, 40.94281824218028 ] } },
+{ "type": "Feature", "properties": { "id": null, "name": "Mt Vernnon High School", "type": "shool" }, "geometry": { "type": "Point", "coordinates": [ -73.8152492016292, 40.925800863183056 ] } },
+{ "type": "Feature", "properties": { "id": null, "name": "New Rochelle High School", "type": "school" }, "geometry": { "type": "Point", "coordinates": [ -73.793921646778628, 40.929984935082324 ] } },
+{ "type": "Feature", "properties": { "id": null, "name": "Crestwood station", "type": "station" }, "geometry": { "type": "Point", "coordinates": [ -73.821128942581737, 40.958795907324905 ] } },
+{ "type": "Feature", "properties": { "id": null, "name": "Greenvale school", "type": "school" }, "geometry": { "type": "Point", "coordinates": [ -73.805816067659094, 40.972843490903678 ] } },
+{ "type": "Feature", "properties": { "id": null, "name": "Leewood park", "type": "park" }, "geometry": { "type": "Point", "coordinates": [ -73.813336666551848, 40.964697886958831 ] } }
+]
+}
